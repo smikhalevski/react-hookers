f9eda5770607414a34a425ad86d61075ae65ed78
Kirill Anisimov <akpoflash@gmail.com>
2021-07-23 11:26:37 +0300

    Add example of useExecution

--- src/main/useExecution.ts
+++ src/main/useExecution.ts
@@ -6,6 +6,27 @@ const NO_DEPS: React.DependencyList = [];
 
 /**
  * Executes a callback when dependencies are changed and returns an {@link IExecution}.
+ *
+ * ```tsx
+ * const [deps, setDeps] = useState();
+ *
+ * const executor = useExecution(async () => {
+ *   // Do some request here fetch(…)
+ * }, [deps]);
+ *
+ * const handleSubmit = () => {
+ *   // Change deps
+ *   // ...
+ *   setDeps(changedDeps);
+ * };
+ *
+ * <button
+ *   onClick={handleSubmit}
+ *   disabled={executor.pending}
+ * >
+ *   {'Submit'}
+ * </button>
+ * ```
  */
 export function useExecution<T>(cb: ExecutorCallback<T>, deps?: React.DependencyList): IExecution<T> {
   const executor = useExecutor<T>();
