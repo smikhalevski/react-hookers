35ad2eaadf475fe91e6435aa013091d5929a2f94
Savva Mikhalevski <smikhalevski@gmail.com>
2021-12-14 20:59:25 +0300

    Fixed useSemanticCallback signature

--- src/main/createSemanticMemoHook.ts
+++ src/main/createSemanticMemoHook.ts
@@ -1,18 +1,18 @@
-import {DependencyList, useMemo} from 'react';
+import {DependencyList} from 'react';
 import {areHookInputsEqual} from './areHookInputsEqual';
 
-export type SemanticMemoHook = typeof useMemo;
+export type SemanticMemoHook = (cb: () => unknown, deps: DependencyList | undefined) => any;
 
 export function createSemanticMemoHook(callable: boolean): SemanticMemoHook {
 
   let prevDeps: DependencyList | undefined;
   let value: any;
 
-  return (factory, deps) => {
+  return (cb, deps) => {
     if (areHookInputsEqual(deps, prevDeps)) {
       return value;
     }
     prevDeps = deps;
-    return value = callable ? factory : factory();
+    return value = callable ? cb : cb();
   };
 }
--- src/main/useSemanticCallback.ts
+++ src/main/useSemanticCallback.ts
@@ -5,7 +5,7 @@ import {createSemanticMemoHook, SemanticMemoHook} from './createSemanticMemoHook
  * A semantic guarantee drop-in replacement for `React.useCallback`. It guarantees that the `cb` won't be "forgotten"
  * until the hook is unmounted.
  */
-export function useSemanticCallback<T>(cb: () => T, deps?: DependencyList): T {
+export function useSemanticCallback<T extends (...args: unknown[]) => unknown>(cb: T, deps?: DependencyList): () => T {
   const hook = useRef<SemanticMemoHook>().current ||= createSemanticMemoHook(true);
   return hook(cb, deps);
 }
