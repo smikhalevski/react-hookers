50fb3945821611d0b9de028fc8e8daa371f7d9c0
Savva Mikhalevski <smikhalevski@gmail.com>
2021-12-16 01:09:43 +0300

    Added useRenderedValueRef hook

---
+++ src/main/ref/useRenderedValueRef.ts
@@ -0,0 +1,27 @@
+import {RefObject, useRef} from 'react';
+
+/**
+ * Creates ref-like object that keeps ref to the given value. It comes handy if you want to use props in the async
+ * context but want to use the actual values but not the ones captured during the render when async context emerged.
+ *
+ * @example
+ * const Foo: React.FC<{ onDone(res: Response): void }> = (props) => {
+ *   const propsRef = useRenderedValueRef(props);
+ *
+ *   React.useEffect(() => {
+ *     (async () => {
+ *       const res = await fetch(…);
+ *
+ *       // Use the latest provided callback even in if the rerender occurred during the await.
+ *       propsRef.current.onDone(res)
+ *     })();
+ *   }, []);
+ *
+ *   return null;
+ * }
+ */
+export function useRenderedValueRef<T>(value: T): RefObject<T> {
+  const ref = useRef(value);
+  ref.current = value;
+  return ref;
+}
--- src/test/ref/useRefCallback.test.ts
+++ src/test/ref/useRefCallback.test.ts
@@ -1,5 +1,5 @@
 import {renderHook} from '@testing-library/react-hooks/native';
-import {useRefCallback} from '../main';
+import {useRefCallback} from '../../main';
 
 describe('useRefCallback', () => {
 
