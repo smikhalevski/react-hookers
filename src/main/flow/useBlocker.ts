5e55c2fd2a2b9e1d37c5e2c12603c4ceb7932fc9
Savva Mikhalevski <smikhalevski@gmail.com>
2022-01-31 20:52:06 +0300

    Moved useBlocker to flow

--- src/main/flow/useBlocker.ts
+++ src/main/flow/useBlocker.ts
@@ -7,8 +7,6 @@ import {useEffectOnce} from '../effect';
  * Blocks UI from the async context.
  *
  * @template T The type of value that can be passed to `unblock` to resolve the `Promise` returned by `block`.
- *
- * @see {@link Lock}
  */
 export function useBlocker<T = void>(): Blocker<T> {
   const rerender = useRerender();
