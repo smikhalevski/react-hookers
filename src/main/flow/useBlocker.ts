2bd58a135758a8c9b49be7bf52575e674eb4285f
smikhalevski <smikhalevski@gmail.com>
2022-03-31 20:33:38 +0300

    useBlocker returns protocol

--- src/main/flow/useBlocker.ts
+++ src/main/flow/useBlocker.ts
@@ -1,18 +1,37 @@
-import {useRef} from 'react';
-import {useRerender} from '../render';
 import {Blocker} from 'parallel-universe';
+import {EffectCallback, useRef} from 'react';
 import {useEffectOnce} from '../effect';
+import {useRerender} from '../render';
+
+export type BlockerProtocol<T> = [blocked: boolean, block: () => Promise<T>, unblock: (result: T) => void];
 
 /**
  * Blocks UI from the async context.
  *
  * @template T The type of value that can be passed to `unblock` to resolve the `Promise` returned by `block`.
  */
-export function useBlocker<T = void>(): Blocker<T> {
+export function useBlocker<T = void>(): Readonly<BlockerProtocol<T>> {
   const rerender = useRerender();
-  const blocker = useRef<Blocker<T>>().current ||= new Blocker<T>();
+  const manager = useRef<ReturnType<typeof createBlockerManager>>().current ||= createBlockerManager(rerender);
 
-  useEffectOnce(() => blocker.subscribe(rerender));
+  useEffectOnce(manager.__effect);
 
-  return blocker;
+  return manager.__protocol;
+}
+
+export function createBlockerManager(rerender: () => void) {
+
+  const blocker = new Blocker();
+
+  const __effect: EffectCallback = () => blocker.subscribe(() => {
+    __protocol[0] = blocker.blocked;
+    rerender();
+  });
+
+  const __protocol: BlockerProtocol<any> = [false, blocker.block.bind(blocker), blocker.unblock.bind(blocker)];
+
+  return {
+    __effect,
+    __protocol,
+  };
 }
