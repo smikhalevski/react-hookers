012aab9e42af4a9b8eca0d0bb0788b9340ad3676
smikhalevski <smikhalevski@gmail.com>
2022-06-14 23:13:59 +0300

    Added useHandler

---
+++ src/main/ref/useHandler.ts
@@ -0,0 +1,19 @@
+import {useRef} from 'react';
+
+/**
+ * Defines an always-stable function identity.
+ */
+export function useHandler<A extends any[], T>(handler: (...args: A) => T): (...args: A) => T;
+
+/**
+ * Defines an always-stable function identity.
+ */
+export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | undefined): (...args: A) => T | undefined;
+
+export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | undefined) {
+  const handlerRef = useRef(handler);
+
+  handlerRef.current = handler;
+
+  return useRef<(...args: A) => any>().current ||= ((...args) => handlerRef.current?.(...args));
+}
