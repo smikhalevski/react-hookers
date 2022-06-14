8b3c290490ce6abcd27cebab012c266350bb559d
smikhalevski <smikhalevski@gmail.com>
2022-06-14 23:24:49 +0300

    Better typing

--- src/main/ref/useHandler.ts
+++ src/main/ref/useHandler.ts
@@ -8,9 +8,9 @@ export function useHandler<A extends any[], T>(handler: (...args: A) => T): (...
 /**
  * Defines an always-stable function identity.
  */
-export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | undefined): (...args: A) => T | undefined;
+export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | null | undefined): (...args: A) => T | undefined;
 
-export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | undefined) {
+export function useHandler<A extends any[], T>(handler: ((...args: A) => T) | null | undefined) {
   const handlerRef = useRef(handler);
 
   handlerRef.current = handler;
