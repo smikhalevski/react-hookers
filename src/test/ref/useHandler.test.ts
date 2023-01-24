9c0605be095261b72c0f91d58fc5cb3a2f3940b9
smikhalevski <smikhalevski@gmail.com>
2023-01-24 15:21:41 +0300

    Added useHandler tests for StrictMode

--- src/test/ref/useHandler.test.ts
+++ src/test/ref/useHandler.test.ts
@@ -1,16 +1,16 @@
-import {renderHook} from '@testing-library/react';
-import {useHandler} from '../../main';
+import { renderHook } from '@testing-library/react';
+import { useHandler } from '../../main';
+import { StrictMode } from 'react';
 
 describe('useHandler', () => {
-
-  test('returns function', () => {
-    const hook = renderHook(() => useHandler(() => 123));
+  test('returns a function', () => {
+    const hook = renderHook(() => useHandler(() => 111), { wrapper: StrictMode });
 
     expect(hook.result.current).toEqual(expect.any(Function));
   });
 
   test('returns the same function on each render', () => {
-    const hook = renderHook(() => useHandler(() => 123));
+    const hook = renderHook(() => useHandler(() => 111), { wrapper: StrictMode });
     const fn = hook.result.current;
 
     hook.rerender();
@@ -18,8 +18,8 @@ describe('useHandler', () => {
     expect(hook.result.current).toBe(fn);
   });
 
-  test('returned function replicated the handler signature', () => {
-    const hook = renderHook(() => useHandler((a, b) => a * 2 + b));
+  test('returned function replicates the handler signature', () => {
+    const hook = renderHook(() => useHandler((a, b) => a * 2 + b), { wrapper: StrictMode });
 
     expect(hook.result.current(3, 4)).toBe(10);
   });
