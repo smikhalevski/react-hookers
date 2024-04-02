b52dbdd83fa62de2b427e71bf2fda03bad8ced7e
smikhalevski <smikhalevski@gmail.com>
2024-04-02 20:51:39 +0300

    Simplified useExecution

--- src/main/useExecution.ts
+++ src/main/useExecution.ts
@@ -1,28 +1,17 @@
-import { DependencyList, useEffect } from 'react';
 import { AbortableCallback, Executor } from 'parallel-universe';
+import { DependencyList } from 'react';
 import type { ExecutorOptions } from './types';
 import { useExecutionEffect } from './useExecutionEffect';
-import { useExecutorManager } from './useExecutorManager';
-import { useExecutorSubscription } from './useExecutorSubscription';
-import { useSemanticMemo } from './useSemanticMemo';
+import { useExecutor } from './useExecutor';
 
 export function useExecution<T>(
   cb: AbortableCallback<T>,
   deps?: DependencyList,
   options?: ExecutorOptions
 ): Executor<T> {
-  const manager = useExecutorManager();
-  const executor = useSemanticMemo(() => manager.createExecutor(), [manager]);
+  const executor = useExecutor(cb, options);
 
-  useExecutorSubscription(executor, cb, options);
   useExecutionEffect(executor, cb, deps);
-
-  useEffect(
-    () => () => {
-      manager.disposeExecutor(executor);
-    },
-    [executor]
-  );
 
   return executor;
 }
