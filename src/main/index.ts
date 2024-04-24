956b4fb7e5c858fe47c62a30c9dfb0944aae2245
smikhalevski <smikhalevski@gmail.com>
2024-04-24 14:18:44 +0300

    Moved executors to react-executor

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,27 +1,15 @@
-export { ExecutorManager } from './ExecutorManager';
-export { ExecutorManagerContext } from './ExecutorManagerContext';
 export { useAnimationFrame } from './useAnimationFrame';
 export { useAsyncEffect } from './useAsyncEffect';
 export { useBlocker } from './useBlocker';
 export { useDebouncedState } from './useDebouncedState';
-export { useExecution } from './useExecution';
-export { useExecutor } from './useExecutor';
-export { useExecutorManager } from './useExecutorManager';
 export { useHandler } from './useHandler';
 export { useInterval } from './useInterval';
 export { useLock } from './useLock';
-export { usePolling } from './usePolling';
 export { useRefCallback } from './useRefCallback';
 export { useRerender } from './useRerender';
 export { useRerenderInterval } from './useRerenderInterval';
-export { useSemanticCallback } from './useSemanticCallback';
-export { useSemanticMemo } from './useSemanticMemo';
-export { useSharedExecution } from './useSharedExecution';
-export { useSharedExecutor } from './useSharedExecutor';
 export { useTimeout } from './useTimeout';
-export { useToggle } from './useToggle';
 
-export type { ExecutorOptions, Schedule } from './types';
 export type { AsyncEffectCallback } from './useAsyncEffect';
 export type { DebouncedStateProtocol } from './useDebouncedState';
-export type { ToggleProtocol } from './useToggle';
+export type { Schedule } from './utils';
--- src/main/useExecutorManager.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { ExecutorManager } from './ExecutorManager';
-import { ExecutorManagerContext } from './ExecutorManagerContext';
-
-export function useExecutorManager(): ExecutorManager {
-  return useContext(ExecutorManagerContext);
-}
